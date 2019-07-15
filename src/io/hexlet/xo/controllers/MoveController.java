bcd197da5c01581f986e9aa2e37ca813ff358eb5
Russkin Sergey <russkin.serge@gmail.com>
2019-07-15 12:46:54 +0300

    Create MoveController

---
+++ src/io/hexlet/xo/controllers/MoveController.java
@@ -0,0 +1,11 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Player;
+
+public class MoveController {
+    public void applyFigure(final Field field,final Figure figure,final Player player){
+
+    }
+}

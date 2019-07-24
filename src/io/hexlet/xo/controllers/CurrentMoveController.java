57c24bb82b82418e19c391affcc64b19aad11df4
Russkin Sergey <russkin.serge@gmail.com>
2019-07-24 16:46:36 +0300

    refactor CurrentMoveController

--- src/io/hexlet/xo/controllers/CurrentMoveController.java
+++ src/io/hexlet/xo/controllers/CurrentMoveController.java
@@ -10,16 +10,25 @@ public class CurrentMoveController {
     public Figure currentMove(Field field) throws InvalidPointException {
         int figureCount = 0;
         for (int i = 0; i < field.getSize(); i++) {
-            for (int j = 0; j < field.getSize(); j++) {
-                if (field.getFigure(new Point(i, j)) != null) {
+                figureCount+= countFiguresInTheRow(field,i);
+        }
+        if (figureCount == field.getSize() * field.getSize()) return null;
+        if (figureCount % 2 == 0) {
+            return Figure.X;
+        } else return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final int row) {
+        int figureCount = 0;
+        for (int j = 0; j < field.getSize(); j++) {
+            try {
+                if (field.getFigure(new Point(j, row)) != null) {
                     figureCount++;
                 }
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
             }
-
         }
-        if (figureCount == field.getSize() * field.getSize())return null;
-        if (figureCount % 2 == 0){
-            return Figure.X;
-        }else return Figure.O;
+        return figureCount;
     }
 }

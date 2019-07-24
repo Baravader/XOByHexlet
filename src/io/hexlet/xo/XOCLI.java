da20e3a27e3b8d099723a9022c771f73fa7f28c2
Russkin Sergey <russkin.serge@gmail.com>
2019-07-24 20:38:50 +0300

    create XOCLI

---
+++ src/io/hexlet/xo/XOCLI.java
@@ -0,0 +1,23 @@
+package io.hexlet.xo;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.Player;
+import io.hexlet.xo.view.ConsoleView;
+
+public class XOCLI {
+    public static void main(String[] args) {
+        final String name1 = "Max";
+        final String name2 = "Gleb";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1,Figure.X);
+        players[1] = new Player(name2,Figure.O);
+
+        final Game gameXO = new Game(players,new Field(3),"XOGame");
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+    }
+}

fc1a81909c7acf66a6453318cf11ec0ed91bab16
Russkin Sergey <russkin.serge@gmail.com>
2019-07-29 16:56:18 +0300

    Refactor, add random filling field method

--- tests/io/hexlet/xo/controllers/CurrentMoveControllerTest.java
+++ tests/io/hexlet/xo/controllers/CurrentMoveControllerTest.java
@@ -2,9 +2,12 @@ package io.hexlet.xo.controllers;
 
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -37,19 +40,25 @@ public class CurrentMoveControllerTest {
     @Test
     public void currentMoveIsNothing() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Field field = new Field(3);
         for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
-            field.setFigure(new Point(0, 0), Figure.X);
-            field.setFigure(new Point(1, 0), Figure.O);
-            field.setFigure(new Point(2, 0), Figure.X);
-            field.setFigure(new Point(0, 1), Figure.O);
-            field.setFigure(new Point(1, 1), Figure.X);
-            field.setFigure(new Point(2, 1), Figure.O);
-            field.setFigure(new Point(0, 2), Figure.X);
-            field.setFigure(new Point(1, 2), Figure.O);
-            field.setFigure(new Point(2, 2), Figure.X);
+            fullFieldRandomiser(field);
             assertNull(currentMoveController.currentMove(field));
         }
 
+    }
+
+
+    private Field fullFieldRandomiser(Field field) throws InvalidPointException {
+        final ArrayList<Figure> figures = new ArrayList();
+        Random random = new Random();
+        figures.add(Figure.X);
+        figures.add(Figure.O);
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                field.setFigure(new Point(x,y),figures.get(random.nextInt(2)));
+            }
+        }
+        return field;
     }
 }
